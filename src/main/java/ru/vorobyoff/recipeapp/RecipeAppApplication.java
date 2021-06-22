15f99807e5016d1298b53a87fc9de25e96ba9952
vorobyoff <>
2021-06-22 16:22:46 +0500

    Initial commit

---
+++ src/main/java/ru/vorobyoff/recipeapp/RecipeAppApplication.java
@@ -0,0 +1,14 @@
+package ru.vorobyoff.recipeapp;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class RecipeAppApplication {
+
+	public static void main(String... args) {
+		run(RecipeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/vorobyoff/recipeapp/RecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vorobyoff.recipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
