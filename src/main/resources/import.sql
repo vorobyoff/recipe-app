9e317eaed5289d2389d7eb91c5a9744fc192bf6d
vorobyoff <>
2021-06-30 10:43:43 +0500

    Added necessary units. Close #11.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -12,12 +12,18 @@ VALUES ('Tablespoon');
 INSERT INTO unit_of_measure (description)
 VALUES ('Teaspoon');
 INSERT INTO unit_of_measure (description)
+VALUES ('Pound');
+INSERT INTO unit_of_measure (description)
 VALUES ('Pinch');
 INSERT INTO unit_of_measure (description)
 VALUES ('Ounce');
 INSERT INTO unit_of_measure (description)
-VALUES ('Cup');
-INSERT INTO unit_of_measure (description)
 VALUES ('Clove');
 INSERT INTO unit_of_measure (description)
-VALUES ('Pound');
+VALUES ('Pint');
+INSERT INTO unit_of_measure (description)
+VALUES ('Each');
+INSERT INTO unit_of_measure (description)
+VALUES ('Dash');
+INSERT INTO unit_of_measure (description)
+VALUES ('Cup');
