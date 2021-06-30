f82737064eeb750a0681dbf8cd9106f4b7d05d5c
vorobyoff <>
2021-06-30 10:48:39 +0500

    Added description search. Close #15.

--- src/main/java/ru/vorobyoff/recipeapp/repositories/CategoryRepository.java
+++ src/main/java/ru/vorobyoff/recipeapp/repositories/CategoryRepository.java
@@ -3,5 +3,9 @@ package ru.vorobyoff.recipeapp.repositories;
 import org.springframework.data.repository.CrudRepository;
 import ru.vorobyoff.recipeapp.domain.Category;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByDescription(final String description);
 }
--- src/main/java/ru/vorobyoff/recipeapp/repositories/UnitOfMeasureRepository.java
+++ src/main/java/ru/vorobyoff/recipeapp/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,9 @@ package ru.vorobyoff.recipeapp.repositories;
 import org.springframework.data.repository.CrudRepository;
 import ru.vorobyoff.recipeapp.domain.UnitOfMeasure;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByDescription(final String description);
 }
