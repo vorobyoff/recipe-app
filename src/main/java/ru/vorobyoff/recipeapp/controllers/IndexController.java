c379cc37c992e8334ef351f8337d4e77ab801eef
vorobyoff <>
2021-06-30 10:49:37 +0500

    IndexController.java modified.

--- src/main/java/ru/vorobyoff/recipeapp/controllers/IndexController.java
+++ src/main/java/ru/vorobyoff/recipeapp/controllers/IndexController.java
@@ -1,13 +1,23 @@
 package ru.vorobyoff.recipeapp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.vorobyoff.recipeapp.services.RecipeService;
 
 @Controller
 public class IndexController {
 
+    private final RecipeService recipeService;
+
+    public IndexController(final RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
     @RequestMapping({"", "/", "/index"})
-    public String getIndexPAge() {
+    public String getIndexPAge(final Model model) {
+        final var recipes = recipeService.getRecipes();
+        model.addAttribute("recipes", recipes);
         return "index";
     }
 }
