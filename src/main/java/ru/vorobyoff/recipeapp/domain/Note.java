4e3919ee430b85f9d916248da9658adea1f45de3
vorobyoff <>
2021-07-06 14:58:06 +0500

    Note.java refactored for Lombok.

--- src/main/java/ru/vorobyoff/recipeapp/domain/Note.java
+++ src/main/java/ru/vorobyoff/recipeapp/domain/Note.java
@@ -1,5 +1,10 @@
 package ru.vorobyoff.recipeapp.domain;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -7,8 +12,12 @@ import javax.persistence.Lob;
 import javax.persistence.OneToOne;
 
 import static javax.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
 
+@Data
 @Entity
+@RequiredArgsConstructor
+@NoArgsConstructor(onConstructor_ = @Deprecated, access = PROTECTED)
 public class Note {
 
     @Id
@@ -16,45 +25,7 @@ public class Note {
     private Long id;
     @OneToOne
     private Recipe recipe;
+    @NonNull
     @Lob
     private String recipeNote;
-
-    public Note(final Long id, final Recipe recipe, final String recipeNote) {
-        this.id = id;
-        this.recipe = recipe;
-        this.recipeNote = recipeNote;
-    }
-
-    public Note(final String recipeNote) {
-        this.recipeNote = recipeNote;
-    }
-
-    @Deprecated
-    // Using only for JPA
-    public Note() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(final Long id) {
-        this.id = id;
-    }
-
-    public Recipe getRecipe() {
-        return recipe;
-    }
-
-    public void setRecipe(final Recipe recipe) {
-        this.recipe = recipe;
-    }
-
-    public String getRecipeNote() {
-        return recipeNote;
-    }
-
-    public void setRecipeNote(final String recipeNote) {
-        this.recipeNote = recipeNote;
-    }
 }
