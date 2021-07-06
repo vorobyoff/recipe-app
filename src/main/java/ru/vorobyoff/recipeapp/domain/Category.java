bc65ca2f8d13077ce78feccf6d8101602e0f599c
vorobyoff <>
2021-07-06 14:56:45 +0500

    Category.java refactored for Lombok.

--- src/main/java/ru/vorobyoff/recipeapp/domain/Category.java
+++ src/main/java/ru/vorobyoff/recipeapp/domain/Category.java
@@ -1,5 +1,10 @@
 package ru.vorobyoff.recipeapp.domain;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -7,49 +12,20 @@ import javax.persistence.ManyToMany;
 import java.util.Set;
 
 import static javax.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
 
+@Data
 @Entity
+@RequiredArgsConstructor
+@NoArgsConstructor(onConstructor_ = @Deprecated, access = PROTECTED)
 public class Category {
 
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
+    @NonNull
     private String description;
+    @NonNull
     @ManyToMany(mappedBy = "categories")
     private Set<Recipe> recipes;
-
-    public Category(final Long id, final String description, final Set<Recipe> recipes) {
-        this.id = id;
-        this.description = description;
-        this.recipes = recipes;
-    }
-
-    @Deprecated
-    // Using only for JPA
-    public Category() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(final Long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(final String description) {
-        this.description = description;
-    }
-
-    public Set<Recipe> getRecipes() {
-        return recipes;
-    }
-
-    public void setRecipes(final Set<Recipe> recipes) {
-        this.recipes = recipes;
-    }
 }
