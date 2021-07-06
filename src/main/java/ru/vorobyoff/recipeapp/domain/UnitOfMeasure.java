1be2920d2064cc1d5e99a5021035b9a4e8720bcc
vorobyoff <>
2021-07-06 14:59:07 +0500

    UnitOfMeasure.java refactored for Lombok.

--- src/main/java/ru/vorobyoff/recipeapp/domain/UnitOfMeasure.java
+++ src/main/java/ru/vorobyoff/recipeapp/domain/UnitOfMeasure.java
@@ -1,42 +1,26 @@
 package ru.vorobyoff.recipeapp.domain;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 import static javax.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
 
+@Data
 @Entity
+@RequiredArgsConstructor
+@NoArgsConstructor(onConstructor_ = @Deprecated, access = PROTECTED)
 public class UnitOfMeasure {
 
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
+    @NonNull
     private String description;
-
-    public UnitOfMeasure(final Long id, final String description) {
-        this.id = id;
-        this.description = description;
-    }
-
-    @Deprecated
-    // Using only for JPA
-    public UnitOfMeasure() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(final Long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(final String uom) {
-        this.description = uom;
-    }
 }
