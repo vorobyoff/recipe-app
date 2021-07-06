c05fb8d87d473d2eb9ed3f07361111427d9414c7
vorobyoff <>
2021-07-06 14:57:25 +0500

    Ingredient.java refactored for Lombok.

--- src/main/java/ru/vorobyoff/recipeapp/domain/Ingredient.java
+++ src/main/java/ru/vorobyoff/recipeapp/domain/Ingredient.java
@@ -1,5 +1,10 @@
 package ru.vorobyoff.recipeapp.domain;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -8,77 +13,25 @@ import javax.persistence.OneToOne;
 import java.math.BigDecimal;
 
 import static javax.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
 
+@Data
 @Entity
+@RequiredArgsConstructor
+@NoArgsConstructor(onConstructor_ = @Deprecated, access = PROTECTED)
 public class Ingredient {
 
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
+    @NonNull
     private String description;
+    @NonNull
     private BigDecimal amount;
+    @NonNull
     @ManyToOne
     private Recipe recipe;
+    @NonNull
     @OneToOne
     private UnitOfMeasure uom;
-
-    public Ingredient(final Long id, final String description, final BigDecimal amount, final Recipe recipe, final UnitOfMeasure uom) {
-        this.id = id;
-        this.description = description;
-        this.amount = amount;
-        this.recipe = recipe;
-        this.uom = uom;
-    }
-
-    public Ingredient(final String description, final BigDecimal amount, final UnitOfMeasure uom, final Recipe recipe) {
-        this.description = description;
-        this.amount = amount;
-        this.recipe = recipe;
-        this.uom = uom;
-    }
-
-    @Deprecated
-    // Using only for JPA
-    public Ingredient() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(final Long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(final String description) {
-        this.description = description;
-    }
-
-    public BigDecimal getAmount() {
-        return amount;
-    }
-
-    public void setAmount(final BigDecimal amount) {
-        this.amount = amount;
-    }
-
-    public Recipe getRecipe() {
-        return recipe;
-    }
-
-    public void setRecipe(final Recipe recipe) {
-        this.recipe = recipe;
-    }
-
-    public UnitOfMeasure getUom() {
-        return uom;
-    }
-
-    public void setUom(final UnitOfMeasure uom) {
-        this.uom = uom;
-    }
 }
