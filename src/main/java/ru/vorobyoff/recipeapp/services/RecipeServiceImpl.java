cb14918cebdf8b4b7a89596fa3017ab4bd1c1ab0
vorobyoff <>
2021-07-13 21:00:31 +0500

    RecipeService.getRecipeById method added in RecipeService.java and it's implementations.

--- src/main/java/ru/vorobyoff/recipeapp/services/RecipeServiceImpl.java
+++ src/main/java/ru/vorobyoff/recipeapp/services/RecipeServiceImpl.java
@@ -1,11 +1,14 @@
 package ru.vorobyoff.recipeapp.services;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 import ru.vorobyoff.recipeapp.domain.Recipe;
 import ru.vorobyoff.recipeapp.repositories.RecipeRepository;
 
 import java.util.HashSet;
 import java.util.Set;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @Service
 public class RecipeServiceImpl implements RecipeService {
@@ -21,5 +24,10 @@ public class RecipeServiceImpl implements RecipeService {
         final var recipes = new HashSet<Recipe>();
         repository.findAll().iterator().forEachRemaining(recipes::add);
         return recipes;
+    }
+
+    @Override
+    public Recipe getRecipeById(final Long id) {
+        return repository.findById(id).orElseThrow(() -> new ResponseStatusException(NOT_FOUND, "Recipe with the given id does not exist."));
     }
 }
