f397407a59adf6d2e85f14330a797690f8bed20c
vorobyoff <>
2021-07-15 22:00:43 +0500

    UnitOfMeasureCommand.java added. Close #25.

---
+++ src/main/java/ru/vorobyoff/recipeapp/commands/UnitOfMeasureCommand.java
@@ -0,0 +1,13 @@
+package ru.vorobyoff.recipeapp.commands;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UnitOfMeasureCommand {
+
+    private String description;
+}
