6a6eb5f1f556f7586b4704b550777633e5726b57
vorobyoff <>
2021-07-25 02:22:01 +0500

    Added empty constructors to BaseCommand and UnitOfMeasureCommand.

--- src/main/java/ru/vorobyoff/recipeapp/commands/BaseCommand.java
+++ src/main/java/ru/vorobyoff/recipeapp/commands/BaseCommand.java
@@ -2,14 +2,16 @@ package ru.vorobyoff.recipeapp.commands;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import static lombok.AccessLevel.PROTECTED;
 
 @Getter
 @Setter
+@NoArgsConstructor(access = PROTECTED)
 @AllArgsConstructor(access = PROTECTED)
-public class BaseCommand {
+public abstract class BaseCommand {
 
     private Long id;
 }
--- src/main/java/ru/vorobyoff/recipeapp/commands/UnitOfMeasureCommand.java
+++ src/main/java/ru/vorobyoff/recipeapp/commands/UnitOfMeasureCommand.java
@@ -2,10 +2,12 @@ package ru.vorobyoff.recipeapp.commands;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public final class UnitOfMeasureCommand extends BaseCommand {
 
     private String description;
