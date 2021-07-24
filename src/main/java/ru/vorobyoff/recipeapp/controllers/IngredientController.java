cddd38153afb7a4d1a652065de5b50684cfed62f
vorobyoff <>
2021-07-25 02:25:23 +0500

    Update functinalty added.

--- src/main/java/ru/vorobyoff/recipeapp/controllers/IngredientController.java
+++ src/main/java/ru/vorobyoff/recipeapp/controllers/IngredientController.java
@@ -4,9 +4,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import ru.vorobyoff.recipeapp.commands.IngredientCommand;
 import ru.vorobyoff.recipeapp.services.IngredientService;
 import ru.vorobyoff.recipeapp.services.UnitOfMeasureService;
+
+import static java.lang.String.format;
 
 @Controller
 @RequiredArgsConstructor
@@ -33,5 +38,14 @@ public final class IngredientController {
         model.addAttribute("units", uomService.findAllUnitCommands());
 
         return "recipe/ingredient/form";
+    }
+
+    @PostMapping("recipe/{recipeId}/ingredient")
+    public final String saveOrUpdateIngredient(final @ModelAttribute IngredientCommand command, @PathVariable final Long recipeId) {
+        if (!command.getRecipeId().equals(recipeId))
+            throw new IllegalArgumentException(format("Parameter 'recipeId' (%1d) and 'command.getRecipeId()' (%2d) are not equal.", recipeId, command.getRecipeId()));
+
+        final var savedCommand = ingredientService.saveIngredientCommand(command);
+        return format("redirect:/recipe/%1d/ingredient/%2d/show", savedCommand.getRecipeId(), savedCommand.getId());
     }
 }
